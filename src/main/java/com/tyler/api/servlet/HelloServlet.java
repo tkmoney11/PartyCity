42a5c8e928d9894cf94c20c415164b75622d9b32
tkmoney11 <tylerkim@outlook.com>
2020-12-07 22:24:19 -0800

    Another One CORS

--- src/main/java/com/tyler/api/servlet/HelloServlet.java
+++ src/main/java/com/tyler/api/servlet/HelloServlet.java
@@ -44,6 +44,7 @@ public class HelloServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().append("Goodbye World");
+        response.setContentType("application/json");
     }
 
 }
